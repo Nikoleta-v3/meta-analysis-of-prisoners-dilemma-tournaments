5b4b8eb29b3ae9a4e9471211c863e4f4e987e1bf
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2017-02-28 14:37:45 +0000

    Reader Implementation.
    A script for reading in the data sets based on the summary csv
    and concating everything along with the strategies properties has been implemented.
    
    The script contains:
    
    - A function for retrieving all the axelrod library strategies and
      their properties.
    
      Corresponding tests have been implemented.
    
    - A function to read in the parameters csv.
    
      Corresponding tests for the type, size and list of columns
      have been implemented.
    
    - Reader is a class for reading in the 4 correspoding data sets
      for each row of the parameters csv.
    
      Containg functions for:
    
      - reading noise data
      - reading probend data
      - standar data
      - noise probend data
      - adding the repetitions, size, seed.
    
      All the above have been tested.
    
    - A function for reading each data set.
    
      Reading in the 4 corresponding data sets and adding the
      common values such as repetitions, size, seed.
    
      Correspoding tests have been implemented. Checking that when
      turns are NaN proend has values and the opposite.
    
    In order to import everything and run it in a notebook.
    To time running time and see memory usage.

---
+++ src/read_in.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import axelrod as axl
+
+
+def strategies_properties():
+    """
+     A function that returns a data frame with the strategies of the Axelrod
+     library and for each strategy it's property.
+    """
+    axl_strategies = pd.DataFrame(columns=['Name', 'Stochastic',
+                                           'Memory_depth', 'Makes_use_of_game',
+                                           'Makes_use_of_length'])
+
+    for i, strategy in enumerate(axl.strategies):
+        use_of_game = 0
+        use_of_length = 0
+
+        if 'game' in strategy.classifier['makes_use_of']:
+            use_of_game = 1
+        if 'length' in strategy.classifier['makes_use_of']:
+            use_of_length = 0
+        axl_strategies.loc[i] = [strategy.name,
+                                 strategy.classifier['stochastic'],
+                                 strategy.classifier['memory_depth'],
+                                 use_of_game,
+                                 use_of_length]
+    return axl_strategies
+
+
+def parameters_data_frame():
+    """
+    A function for reading in the parameters data frame.
+    """
+    df = pd.read_csv("../data/parameters.csv")
+    df = df[['noise', 'probend', 'repetitions', 'seed', 'size', 'turns']]
+
+    return df
+
+
+class Reader():
+    """
+    A class for retrieving the four data frames corresponding to each
+    row of the parameters csv file.
+    """
+
+    def __init__(self, row):
+        self.seed = row.seed
+        self.size = row['size']
+        self.repetitions = row.repetitions
+        self.noise_param = row.noise
+        self.probend_param = row.probend
+        self.turns_param = row.turns
+
+    def get_noise_df(self):
+        noise = pd.read_csv('../data/noise-data/{}.csv'.format(int(self.seed)))
+        noise['noise'] = [self.noise_param for _ in range(len(noise))]
+        noise['turns'] = [self.turns_param for _ in range(len(noise))]
+
+        return noise
+
+    def get_probend_noise_df(self):
+        probend_noise = pd.read_csv('../data/noise-probend-data/{}.csv'.format(
+                                                                int(self.seed)))
+        probend_noise['noise'] = [self.noise_param
+                                  for _ in range(len(probend_noise))]
+        probend_noise['probend'] = [self.probend_param
+                                    for _ in range(len(probend_noise))]
+
+        return probend_noise
+
+    def get_standar_df(self):
+        std = pd.read_csv('../data/std-data/{}.csv'.format(int(self.seed)))
+        std['turns'] = [self.turns_param for _ in range(len(std))]
+
+        return std
+
+    def get_proend_df(self):
+        probend = pd.read_csv('../data/probend-data/{}.csv'.format(int(
+                                                                    self.seed)))
+        probend['probend'] = [self.probend_param for _ in range(len(probend))]
+
+        return probend
+
+    def common_rows(self, df):
+        for i in range(len(df)):
+            df[i]['repetitions'] = [self.repetitions for _ in range(len(df[i]))]
+            df[i]['size'] = [self.size for _ in range(len(df[i]))]
+            df[i]['seed'] = [self.seed for _ in range(len(df[i]))]
+
+        return df
+
+
+def reading_in_data(parameters_df):
+    """
+    A function for reading in each of the 4 data sets for a given parameters
+    row. It concats everything together and returns a pandas Data Frame.
+    """
+    data_frame = pd.DataFrame()
+    for i in parameters_df.index:
+        dfs = []
+        row = parameters_df.loc[i]
+        reader = Reader(row)
+
+        # get noise
+        noise = reader.get_noise_df()
+        dfs.append(noise)
+
+        # get probend noise
+        probend_noise = reader.get_probend_noise_df()
+        dfs.append(probend_noise)
+
+        # get standar
+        std = reader.get_standar_df()
+        dfs.append(std)
+
+        # get probend
+        probend = reader.get_proend_df()
+        dfs.append(probend)
+
+        # add the common params
+        dfs = reader.common_rows(dfs)
+
+        dfs = pd.concat(dfs)
+
+        data_frame = data_frame.append(dfs)
+
+    return data_frame
+
+# parameters_df = parameters_data_frame()
+# df = reading_in_data(parameters_df)
+# strategies_df = strategies_properties()
+#
+# final_df = pd.merge(df, strategies_df, on='Name')
+# final_df.to_csv('data.csv')
