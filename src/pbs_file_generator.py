3f1882fe69614c3c6ab00b1368dbb43a3de6fac4
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-05-08 17:13:41 +0100

    clean repo

--- src/pbs_file_generator.py
+++ src/pbs_file_generator.py
@@ -1,3 +1,13 @@
+import os
+
+directory_name = 'pbs'
+ 
+try:
+    os.mkdir(directory_name)
+    print("Directory " , directory_name, " Created")
+except FileExistsError:
+    pass
+
 submit_script = ""
 for name, min_seed, max_seed in [("0-999", 0, 1000),
                                  ("1000-1999", 1000, 2000),
@@ -43,4 +53,6 @@ cd /home/c1569433/rsc/axlml/src
 
 with open("submit_meta_workers.sh", 'w') as f:
     f.write(submit_script)
+    print("File submit_meta_workers.sh Created")
+
 
